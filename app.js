ba505054d63bd743c50002c5f19133f736a2e0c3
vladian95 <kvankvan1995@gmail.com>
2022-06-30 20:35:00 +0300

    Pinning and Animation on Scroll

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
+// Pin the first page
 const tlIntro = gsap.timeline({
     scrollTrigger: {
-        trigger: ".first-page",
+        trigger: '.first-page',
         start: '0%',
-        end: '25%',
-        scrub: true,
-    }, 
+        end: '100%', 
+        pin: true,
+        pinSpacing: false,
+    }
 });
 
-tlIntro.fromTo('nav', {opacity: 1}, {opacity: 0});
+//highlight page two
+const tlH = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.second-page',
+        markers: { startColor: 'blue', endColor: 'blue' },
+        scrub: true,
+        start: '-40%',
+        end: '40%',
+    }
+});
+
+tlH.fromTo(
+    ".highlight",
+    { color: 'rgba(255, 255, 255, 0.4' },
+    { color: 'rgba(255, 255, 255, 1', stagger: 1 }
+);
+
+const tlHRemove = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.second-page',
+        markers: { startColor: 'pink', endColor: 'pink' },
+        scrub: true,
+        start: '-20%',
+        end: '60%',
+    }
+});
+
+tlHRemove.to(".highlight", { color: 'rgba(255, 255, 255, 0.4', stagger: 1 });
