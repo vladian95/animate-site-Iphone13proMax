2d062f419cba7417eda2fbb4288c9160c72dcb92
vladian95 <kvankvan1995@gmail.com>
2022-07-01 19:06:46 +0300

    Carousel

--- app.js
+++ app.js
@@ -66,3 +66,32 @@ const tlSplitPin = gsap.timeline({
         end: '100%',
     }
 })
+
+
+//carousel
+
+const swatches = document.querySelectorAll(".swatches img");
+const gallery = document.querySelector(".phone-gallery");
+const slides = document.querySelectorAll(".phone-gallery-container");
+
+let currentSwatch = "blue";
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+    const coord = slides[index].getBoundingClientRect().left;
+
+    swatch.addEventListener('click', (e) => {
+        let swatchName = e.target.getAttribute("swatch");
+        let closeUp = document.querySelector("." + swatchName);
+
+        if(currentSwatch === swatchName) return;
+
+        gsap.set(closeUp, {zIndex: topIndex});
+        gsap.fromTo(closeUp, {opacity: 0}, {opacity: 1, duration: 1});
+
+        gsap.to(gallery, { x: -coord, duration: 1, ease: "back.out(1)" });
+
+        topIndex ++;
+        currentSwatch = swatchName;
+    });
+});
